cfa261ede3e4e01ba13ac837c1a6470c0bef4839
Anna Grochola <agrochola@cyfrowypolsat.pl>
2018-11-17 15:42:21 +0100

    Property binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { InputButtonUnitComponent } from './input-button-unit/input-button-unit.component';
+import { ToDoItemComponent } from './to-do-item/to-do-item.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    InputButtonUnitComponent
+    InputButtonUnitComponent,
+    ToDoItemComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/to-do-item/to-do-item.component.ts
@@ -0,0 +1,18 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-to-do-item',
+  templateUrl: './to-do-item.component.html',
+  styleUrls: ['./to-do-item.component.css']
+})
+export class ToDoItemComponent implements OnInit {
+
+  @Input() item;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
